023c0e200ed1a754fd557ec94918f56edea06168
Kirill_Egorov <kirill1337egorov2000@gmail.com>
2022-12-11 17:30:40 +0300

    add class

--- lab9/test.js
+++ lab9/test.js
@@ -1,10 +1,10 @@
 ﻿// JavaScript source code
-
 const chai = require('chai');
 const assert = chai.assert
 const baseUrl = 'http://shop.qatl.ru/api';
 const expect = chai.expect;
 const request = require('supertest')(baseUrl)
+
 
 const wrongCategoryId = require('./tests/invalid/wrongCategoryId')
 const wrongTitle = require('./tests/invalid/wrongTitle')
@@ -19,8 +19,53 @@ const wrongHit = require('./tests/invalid/wrongHit')
 const validProduct1 = require('./tests/valid/validProduct1')
 const validProduct2 = require('./tests/valid/validProduct2')
 
-let existingIds = []
-let addedIds = []
+class APIrequests {
+
+    existingIds = []
+    addedIds = []
+    request = require('supertest')(baseUrl)
+
+    async addProduct(product) {
+        const res = await request
+            .post('/addproduct')
+            .send(product)
+        this.addedIds.push(res.body.id)
+        return res;
+    }
+
+    async getProducts() {
+        const res = await request.get('/products')
+        res.body.forEach(product => {
+            this.existingIds.push(product.id)
+        })
+        return res;
+    }
+
+    async editProduct(id, editData) {
+        const res = await request
+            .post('/editproduct')
+            .send({
+                id: id,
+                ...editData,
+            })
+        return res
+    }
+
+    async deleteProduct(id) {
+        const res = await request.get('/deleteproduct?id=' + id)
+        this.addedIds = []
+        this.existingIds = []
+        return res
+    }
+
+    getAddedIds() {
+        return this.addedIds
+    }
+
+    getExistingIds() {
+        return this.existingIds
+    }
+}
 
 function checkFields(first, second) {
     assert(first.category_id == second.category_id, 'id не совпадают')
@@ -34,97 +79,55 @@ function checkFields(first, second) {
     assert(first.hit == second.hit, 'hit не совпадают')
 }
 
-async function addProduct(product) {
-    res = await request
-        .post('/addproduct')
-        .send(product)
-    addedIds.push(res.body.id)
-    return res;
-}
-
-async function getProducts() {
-    res = await request.get('/products')
-    res.body.forEach(product => {
-        existingIds.push(product.id)
-    })
-    return res;
-}
-
-async function editProduct(id, editData) {
-    res = await request
-        .post('/editproduct')
-        .send({
-            id: id,
-            ...editData,
-        })       
-    return res
-}
-
-async function deleteProduct(id) {
-    res = await request.get('/deleteproduct?id=' + id)
-    return res
-}
 describe('Products API', () => {
+    const req = new APIrequests(request);
 
     afterEach(() => {
+        const addedIds = req.getAddedIds()
         addedIds.forEach(async (id) => {
-            const resDel = await deleteProduct(id)
+            const resDel = await req.deleteProduct(id)
         })
-        addedIds = []
-        existingIds = []
     }); 
 
     it('Получение всех продуктов', async () => {
-        const res = await getProducts();
-        assert(res.status === 200, 'Ошибка сервера')
+        const res = await req.getProducts();
         assert(res.status == 200, 'Ошибка сервера')
-        assert(existingIds != [], 'Не получено ни одного товара')
+        assert(req.getExistingIds() != [], 'Не получено ни одного товара')
     })
 
+    
     it('Добавление продукта и проверка его полей', async () => {
-        const res = await addProduct(validProduct1)
-
-        let addedPr
-        const products = await getProducts()
-        products.body.forEach(pr => {
-            if (pr.id == res.body.id) {
-                addedPr = pr;
-            }
-        })
+        const res = await req.addProduct(validProduct1)
+        const products = await req.getProducts()
+        let addedPr = products.body.find(product => product.id == res.body.id);
         assert(res.body.status === 1, 'Статус 0')
         assert(res.status == 200, 'Ошибка сервера')
         assert(addedPr, 'Продукт не существует')
         checkFields(addedPr, validProduct1)
-
     })
     
     it('Редактирование добавленного продукта и проверка правильности измененных полей', async () => {
         const editData = validProduct2
-        const resAdd = await addProduct(validProduct1);
+        const resAdd = await req.addProduct(validProduct1);
         assert(resAdd.body.status === 1, 'Статус 0')
         let currId = resAdd.body.id
-        const resEdit = await editProduct(currId, editData)
-        const res = await getProducts();
+        const resEdit = await req.editProduct(currId, editData)
+        const res = await req.getProducts();
         assert(resEdit.body.status == 1, 'Статус 0')
-        let currProduct
-        res.body.forEach(product => {
-            if (product.id == currId) {
-                currProduct = product;
-            }
-        })
+        let currProduct = res.body.find(product => product.id == currId);
         assert(currProduct.id == currId, 'id добавленного продукта не соотетствует id продукта, который был изменен');
         checkFields(currProduct, editData)
     })     
     
     it('Проверка поля alias', async () => {
-        const firstRes = await addProduct(validProduct1);
+        const firstRes = await req.addProduct(validProduct1);
         assert(firstRes.status == 200)
-        const secondRes = await addProduct(validProduct1);
+        const secondRes = await req.addProduct(validProduct1);
         assert(secondRes.status == 200)
-        const thirdRes = await addProduct(validProduct1);
+        const thirdRes = await req.addProduct(validProduct1);
         assert(thirdRes.status == 200)
 
-        const products = await getProducts();
+        const products = await req.getProducts();
 
         const first = products.body.find(product => product.id == firstRes.body.id);
         const second = products.body.find(product => product.id == secondRes.body.id);
@@ -136,44 +139,44 @@ describe('Products API', () => {
     })
     
     it('Добавление продуктов с невалидными значениями', async () => {
-        res = await addProduct(wrongStatus);
+        res = await req.addProduct(wrongStatus);
         assert(res.status == 200, 'Ошибка сервера')
 
-        res = await addProduct(wrongCategoryId);
+        res = await req.addProduct(wrongCategoryId);
         assert(res.status == 200, 'Ошибка сервера')
 
-        res = await addProduct(wrongHit);
+        res = await req.addProduct(wrongHit);
         assert(res.status == 200, 'Ошибка сервера')
 
-        res = await addProduct(wrongPrice);
+        res = await req.addProduct(wrongPrice);
         assert(res.status == 200, 'Ошибка сервера')
 
-        res = await addProduct(wrongOldPrice);
+        res = await req.addProduct(wrongOldPrice);
         assert(res.status == 200, 'Ошибка сервера')
 
-        res = await addProduct(wrongKeywords);
+        res = await req.addProduct(wrongKeywords);
         assert(res.status == 200, 'Ошибка сервера')
 
-        res = await addProduct(wrongDescription);
+        res = await req.addProduct(wrongDescription);
         assert(res.status == 200, 'Ошибка сервера')
 
         const emptyProduct = {}
-        res = await addProduct(emptyProduct)
+        res = await req.addProduct(emptyProduct)
         assert(res.status == 200, 'Ошибка сервера')
 
-        const allProducts = await getProducts();
+        const allProducts = await req.getProducts();
 
         //при вызове addProduct(product) в addedIds добавляется id 
         allProducts.body.forEach(product => {
-            assert(!(addedIds.includes(product.id)), 'В список товаров был добавлен невалидный товар')
+            assert(!(req.getAddedIds().includes(product.id)), 'В список товаров был добавлен невалидный товар')
         })
     })
     
     it('Удаление несуществующего товара', async () => {
         const awayId = 555;
-        const res = await deleteProduct(awayId);
-        assert(existingIds != [])
-        assert(!(existingIds.includes(awayId)), 'Товар с таким id существует в списке товаров')
+        const res = await req.deleteProduct(awayId);
+        assert(req.getExistingIds() != [])
+        assert(!(req.getExistingIds().includes(awayId)), 'Товар с таким id существует в списке товаров')
         assert(res.status == 200, 'Ошибка сервера')
         assert(res.body.status == 0, 'Статус 1');
     })
@@ -181,48 +184,34 @@ describe('Products API', () => {
     it('Редактирование несуществующего товара', async () => {
         const awayId = 555
         const editData = validProduct2
-        const resEdit = await editProduct(awayId, editData)
+        const resEdit = await req.editProduct(awayId, editData)
         
-        const products = await getProducts();
-        let awayProduct
-        existingIds.forEach(product => {
-            if (product.id = awayId) {
-                awayProduct = product
-            }
-        })
+        const products = await req.getProducts();
+        
+        let awayProduct = products.body.find(product => product.id == awayId);
         assert(resEdit.status == 200, 'Ошибка сервера')
-        assert(awayProduct.id == undefined, 'Отредактировался несуществующий товар')
+        assert(!awayProduct, 'Отредактировался несуществующий товар')
     })
-
+    
     it('Изменение параметров валидного продукта на невалидные данные', async () => {
-        const addRes = await addProduct(validProduct1)
+        const addRes = await req.addProduct(validProduct1)
 
         let currProd
-        let products1 = await getProducts()
-        let currProduct
-        products1.body.forEach(product => {
-            if (product.id == addRes.body.id) {
-                currProduct = product;
-            }
-        })
+        let products1 = await req.getProducts()
+        let currProduct = products1.body.find(product => product.id == addRes.body.id);
 
-        let editRes = await editProduct(currProduct, wrongTitle)
-        editRes = editProduct(currProduct, wrongHit)
-        editRes = editProduct(currProduct, wrongPrice)
-        editRes = editProduct(currProduct, wrongCategoryId)
+        let editRes = await req.editProduct(currProduct, wrongTitle)
+        editRes = await req.editProduct(currProduct, wrongHit)
+        editRes = await req.editProduct(currProduct, wrongPrice)
+        editRes = await req.editProduct(currProduct, wrongCategoryId)
    
-        let products2 = await getProducts()
-        let editedProduct
-        products1.body.forEach(product => {
-            if (product.id == addRes.body.id) {
-                editedProduct = product;
-            }
-        })
+        let products2 = await req.getProducts()
+        let editedProduct = products2.body.find(pr => pr.id == addRes.body.id)
 
         assert(editedProduct.title != wrongTitle.title, 'Продукт изменен на невалидный title')
         assert(editedProduct.hit != wrongHit.hit, 'Продукт изменен на невалидный hit')
         assert(editedProduct.price != wrongPrice.price, 'Продукт изменен на невалидный price')
         assert(editedProduct.category_id != wrongCategoryId.category_id, 'Продукт изменен на невалидный categoryId')
     })
-
+   
 }) 
